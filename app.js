4c0d8b4c9ff577dbf739c9db0c489d8e690e52b9
Juliana <juliana.mg3@gmail.com>
2020-07-14 20:18:35 -0300

    cambios realizados, pero aun siguen persistiendo algunos errores

--- app.js
+++ app.js
@@ -7,49 +7,68 @@ const ULTIMO_NIVEL = 10
 
 class Juego {
   constructor() {
-      setTimeout(() => {
-        this.inicializar()
-        this.generarSecuencia()
-        this.siguienteNivel()
-      }, 500)    
       
-      inicializar() {
-      btnEmpezar.classList.add('hide')
-      this.nivel = 1
-      this.colores = {
-          celeste,
-          violeta,
-          naranja,
-          verde
+          this.inicializar = this.inicializar.bind(this)
+          this.inicializar()
+          this.generarSecuencia()
+          setTimeout(() => this.siguienteNivel(), 500)
+    }    
+          
+     inicializar() {
+               this.siguienteNivel = this.siguienteNivel.bind(this)
+               this.elegirColor = this.elegirColor.bind(this)          
+               btnEmpezar.classList.add('hide')
+               this.nivel = 1
+               this.colores = {
+               celeste,
+               violeta,
+               naranja,
+               verde
+             }
+             
         }
-     }
-     
      generarSecuencia() {
-      this.generarSecuencia = new Array(10).fill(0).map(n => Math.floor(Math.random( * 4)))
+         this.secuencia = new Array(10)
+         .fill(0)
+         .map(n => Math.floor(Math.random() * 4))
      }
 
      siguienteNivel() {
+         this.subnivel = 0
          this.iluminarSecuencia()
+         this.agregarEventosClick()
      }
 
-     transformarNumeroAColor(color) {
+     transformarColorANumero(color) {
         switch (color) {
-        case 'Celeste':
+        case 'celeste':
          return 0
         case 'violeta':
          return 1
-        case 'Naranja':
+        case 'naranja':
          return 2
-        case 'Verde':
-          return 4
+        case 'verde':
+          return 3
         }
+     }
+
+     transformarNumeroAColor(numero){
+         switch(numero) {
+             case 0:
+                 return 'celeste'
+            case 1: 
+                return 'violeta'
+            case 2:
+                return 'naranja'
+            case 3: 
+                return 'verde'
+         }
      }
 
      iluminarSecuencia() {
         for (let i = 0; i < this.nivel; i++) {
-            let color = this.transformarNumeroAColor(this.secuencia[i])
-            this.iluminarColor(color)
-            setTimeout(() => IluminarColor(color), 1000 * i)
+            const color = this.transformarNumeroAColor(this.secuencia[i])
+            setTimeout(() => this.iluminarColor(color), 1000 * i)
         }
      }
 
@@ -57,18 +76,17 @@ class Juego {
         this.colores[color].classList.add('light')
         setTimeout(() => this.apagarColor(color), 350)
      }
-
      apagarColor(color) {
         this.colores[color].classList.remove('light')
      }
-     
+
      agregarEventosClick() {
         this.colores.celeste.addEventListener('click', this.elegirColor)
         this.colores.verde.addEventListener('click', this.elegirColor)
         this.colores.violeta.addEventListener('click', this.elegirColor)
         this.colores.naranja.addEventListener('click', this.elegirColor)
      }
-    
+
      eliminarEvenosClick() {
         this.colores.celeste.removeEventListener('click', this.elegirColor)
         this.colores.verde.removeEventListener('click', this.elegirColor)
@@ -88,7 +106,7 @@ class Juego {
              if (this.nivel === (ULTIMO_NIVEL + 1)) {
                 this.ganoEljuego()
             } else {
-                setTimeOut(this.siguienteNivel, 1500)
+                setTimeout(this.siguienteNivel, 1500)
             }
     
             }
@@ -100,15 +118,21 @@ class Juego {
         ganoEljuego() {
             swal('Platzi', 'Felicitaciones, ganaste el juego!', 'success')
     
+        }
+        perdioEljuego() {
+            swal({
+                title: 'Simon Dice:',
+                text: 'Lo Siento! Perdiste!',
+                icon: 'error',
+                button: 'volver a intentar',
+            }).then(() => {
+                this.eliminarEvenosClick()
+                this.inicializar()
+            })
         }    
-
-    }
-
-
-
-       
+    
+    }       
       
-} 
       
 function empezarJuego() {
     window.juego = new Juego()
